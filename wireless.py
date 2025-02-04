08d8e1fc4768dc5e9190dae425935eccc627b077
Harry <harrychen0805@outlook.com>
2025-02-04 13:10:32 -0330

    gpiod

--- wireless.py
+++ wireless.py
@@ -1,13 +1,23 @@
-from rpi_rf import RFDevice
+import gpiod
 import time
 
-rfdevice = RFDevice(17)  # GPIO Pin connected to Data pin
-rfdevice.enable_tx()
+CHIP = "/dev/gpiochip4"  # Use gpioinfo command to verify the correct chip
+TX_GPIO = 17  # Your transmitter GPIO pin
+
+chip = gpiod.Chip(CHIP)
+line = chip.get_line(TX_GPIO)
+
+config = gpiod.LineRequest()
+config.consumer = "rf_tx"
+config.request_type = gpiod.LINE_REQ_DIR_OUT
+
+line.request(config)
 
 try:
     while True:
-        rfdevice.tx_code(1234)  # Send signal
-        print("Signal sent!")
+        line.set_value(1)  # Send HIGH signal
+        time.sleep(0.5)
+        line.set_value(0)  # Send LOW signal
         time.sleep(2)
 except KeyboardInterrupt:
-    rfdevice.cleanup()
+    line.release()
